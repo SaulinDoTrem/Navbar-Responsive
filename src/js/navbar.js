ab7afd3bb757355aaf7051f0efd94435aef45fce
Saulo Klein Nery <saulokn@outlook.com>
2022-10-26 14:42:34 -0300

    feat: Add Color Flipper to the link change color on hover

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,8 +1,7 @@
 let showLinks = false;
 
-$('.nav-toggle').on("click",
+$('.nav-toggle').click(
     () => {
-        console.log("Apertei o botão");
         if(!showLinks)
             $('.page-links').addClass('show-links');
         else
@@ -11,3 +10,30 @@ $('.nav-toggle').on("click",
         showLinks = !showLinks;
     }
 );
+
+$('.page-link').hover(
+    function(){
+        const randomColor = getRandomColor();
+        $(this).css('background-color', randomColor);
+    },
+    function(){
+        $(this).css('background-color', '#FFF');
+    }
+)
+
+function getRandomColor(){
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let color = '#';
+
+    for (let index = 0; index < 6; index++) {
+        const randomNumber = getRandomNumber(hex);
+        const caracter = hex[randomNumber];
+        color += caracter;
+    }
+
+    return color;
+}
+
+function getRandomNumber(array){
+    return Math.floor(Math.random() * array.length);
+}
